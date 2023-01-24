fe67101e98e37cd596a352a380a81c8f3dd8c8e3
Erik M Baxstrom <baxstrem@gmail.com>
2023-01-24 15:43:36 -0800

    add regex to ensure rgb values are numbers 0-255

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import NavBar from './components/NavBar/NavBar.js';
 import NotFound from './components/NotFound/NotFound.js';
 
 function App() {
+  const filter = '([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])';
+  const red = `:redValue${filter}`;
+  const green = `:greenValue${filter}`;
+  const blue = `:blueValue${filter}`;
+
   return (
     <div className="App">
       <header>
@@ -18,7 +23,7 @@ function App() {
             <p>Choose a color from the links above, or enter your own RGB values into the URL.</p>
           </div>
         </Route>
-        <Route exact path="/rgb/:redValue/:greenValue/:blueValue" component={ColorDisplay}></Route>
+        <Route exact path={`/rgb/${red}/${green}/${blue}`} component={ColorDisplay}></Route>
         <Route path="*" component={NotFound} />
       </Switch>
     </div>
